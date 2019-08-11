d235ca124473de8f8963bf120a60eae2fe5a7f6c
Daigo Hamasaki <daigo.hamasaki.16@gmail.com>
2019-08-11 20:17:53 +0900

    Enable to connect to GitHub's GraphQL API

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { ApolloProvider } from '@apollo/react-hooks';
 import './App.css';
 import SearchPage from './components/pages/SearchPage';
+import client from './apolloClient';
 import store from './store';
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <div className="App">
-        <SearchPage />
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <SearchPage />
+        </div>
+       </ApolloProvider>
     </Provider>
   );
 }
