9749f279c3087b4203324d5fd352d5c4e7b3ce13
Daigo Hamasaki <daigo.hamasaki.16@gmail.com>
2019-08-18 23:02:13 +0900

    Add tests for checking the tag in the component

--- src/components/atoms/MenuButton.test.tsx
+++ src/components/atoms/MenuButton.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import MenuButton from './MenuButton';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<MenuButton />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('has one button tag', () => {
+  const wrapper = shallow(<MenuButton />);
+  expect(wrapper.find('button').length).toBe(1);
 });
--- src/components/molecules/PageTitle.test.tsx
+++ src/components/molecules/PageTitle.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import PageTitle from './PageTitle';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<PageTitle />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('has one h1 tag', () => {
+  const wrapper = shallow(<PageTitle />);
+  expect(wrapper.find('h1').length).toBe(1);
 });
