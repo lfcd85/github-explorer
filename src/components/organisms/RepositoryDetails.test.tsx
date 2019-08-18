90c7333802850b97dc891a87d5b3e2b64fdb3901
Daigo Hamasaki <daigo.hamasaki.16@gmail.com>
2019-08-18 22:30:21 +0900

    Add tests for SearchPage

--- src/components/organisms/RepositoryDetails.test.tsx
+++ src/components/organisms/RepositoryDetails.test.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import RepositoryDetails from './RepositoryDetails';
 
+export const mockRepositoryDetails = { // eslint-disable-line import/prefer-default-export
+  id: 'id',
+  nameWithOwner: 'nameWithOwner',
+  isArchived: false,
+  languages: { edges: [] },
+  description: 'description',
+  forkCount: 777,
+  createdAt: '2017-07-07T07:07:07Z',
+  updatedAt: '2018-08-08T08:08:08Z',
+  url: 'dummy url',
+};
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  const mockRepositoryDetails = {
-    id: 'id',
-    nameWithOwner: 'nameWithOwner',
-    isArchived: false,
-    languages: { edges: [] },
-    description: 'description',
-    forkCount: 777,
-    createdAt: '2017-07-07T07:07:07Z',
-    updatedAt: '2018-08-08T08:08:08Z',
-    url: 'dummy url',
-  };
 
   ReactDOM.render(<RepositoryDetails repositoryDetails={mockRepositoryDetails} />, div);
   ReactDOM.unmountComponentAtNode(div);
--- src/components/pages/SearchPage.test.tsx
+++ src/components/pages/SearchPage.test.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import SearchPage from './SearchPage';
 import App from '../App';
+import PageTitle from '../molecules/PageTitle';
+import RepositoryList from '../organisms/RepositoryListContainer';
+import RepositoryDetails from '../organisms/RepositoryDetailsContainer';
+import { mockRepositoryDetails } from '../organisms/RepositoryDetails.test';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -11,4 +16,21 @@ it('renders without crashing', () => {
     </App>
   ), div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders one PageTitle component', () => {
+  const wrapper = shallow(<SearchPage />);
+  expect(wrapper.find(PageTitle).length).toBe(1);
+});
+
+it('renders one RepositoryList component when repository is not selected', () => {
+  const wrapper = shallow(<SearchPage repositoryDetails={{}} />);
+  expect(wrapper.find(RepositoryList).length).toBe(1);
+  expect(wrapper.find(RepositoryDetails).length).toBe(0);
+});
+
+it('renders one RepositoryDetails component when repository is selected', () => {
+  const wrapper = shallow(<SearchPage repositoryDetails={mockRepositoryDetails} />);
+  expect(wrapper.find(RepositoryDetails).length).toBe(1);
+  expect(wrapper.find(RepositoryList).length).toBe(0);
 });
