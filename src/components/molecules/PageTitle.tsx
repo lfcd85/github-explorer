c4b340d5ef66075be1b85f959802cc7b9edcdec9
Daigo Hamasaki <daigo.hamasaki.16@gmail.com>
2019-08-18 14:25:55 +0900

    Add link from PageTitle to the site itself

--- src/components/molecules/PageTitle.tsx
+++ src/components/molecules/PageTitle.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './PageTitle.scss';
 
 const PageTitle: React.FC<{ title: string }> = props => (
-  <h1 className="PageTitle">{props.title}</h1>
+  <a href="/">
+    <h1 className="PageTitle">{props.title}</h1>
+  </a>
 );
 
 export default PageTitle;
